10d241d17a3f1ec9db91981610f540f0a1371450
Sawan-k-yadav <sawan.gomia@gmail.com>
2024-08-19 04:09:02 +0530

    code and version updated

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
     long_description = f.read()     
    
 
-__version__ = "0.0.4"
+__version__ = "0.0.2"
 REPO_NAME = "MongoDB-Python-Connector-PYPI-Package"
 PKG_NAME= "databaseautomation"
 AUTHOR_USER_NAME = "Sawan Yadav"
